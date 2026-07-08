cf1388e7690653a6e494bf8c406602b2cd72ba84
allwin benhail <allwin.benhail@gmail.com>
2026-07-08 14:05:01 +0530

    git commit -m "Initial solution setup with API, Azure Functions, Shared library and NUnit test project"

---
+++ test/AzureMessageProcessing.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace AzureMessageProcessing.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
